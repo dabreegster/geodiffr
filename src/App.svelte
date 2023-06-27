65024c685320f5db2441448597d00111b0fdcff2
Dustin Carlino <dabreegster@gmail.com>
2023-06-27 16:35:08 +0100

    Copy in some scaffolding from osm2svelte

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Layout from "./Layout.svelte";
+</script>
+
+<Layout>
+  <div slot="left">
+    <h1>GeoDiffr</h1>
+  </div>
+  <div slot="main">
+    <p>Map goes here</p>
+  </div>
+</Layout>
