df57b47085d97e36674897ee04adfe3976df42b0
Dustin Carlino <dabreegster@gmail.com>
2023-06-27 17:14:22 +0100

    Try svelte-maplibre

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { MapLibre } from "svelte-maplibre";
   import Layout from "./Layout.svelte";
 </script>
 
@@ -6,7 +7,10 @@
   <div slot="left">
     <h1>GeoDiffr</h1>
   </div>
-  <div slot="main">
-    <p>Map goes here</p>
+  <div slot="main" style="position:relative; width: 100%; height: 100vh;">
+    <MapLibre
+      style="https://api.maptiler.com/maps/streets/style.json?key=MZEJTanw3WpxRvt7qDfo"
+      standardControls
+    />
   </div>
 </Layout>
