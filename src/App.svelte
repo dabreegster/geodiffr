a85fa6ec0351c87b9e3a3b5bc01a85770911c14a
Dustin Carlino <dabreegster@gmail.com>
2023-06-27 17:34:12 +0100

    Display some Overpass output

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
-  import { MapLibre } from "svelte-maplibre";
+  import { onMount } from "svelte";
+  import { GeoJSON, LineLayer, MapLibre } from "svelte-maplibre";
   import Layout from "./Layout.svelte";
+
+  let sampleData;
+  onMount(async () => {
+    let resp = await fetch("/geodiffr/overpass_example.geojson");
+    sampleData = await resp.json();
+  });
 </script>
 
 <Layout>
@@ -10,7 +17,15 @@
   <div slot="main" style="position:relative; width: 100%; height: 100vh;">
     <MapLibre
       style="https://api.maptiler.com/maps/streets/style.json?key=MZEJTanw3WpxRvt7qDfo"
+      center={[-0.1095, 51.5076]}
+      zoom={13}
       standardControls
-    />
+    >
+      {#if sampleData}
+        <GeoJSON id="data" data={sampleData}>
+          <LineLayer paint={{ "line-width": 5, "line-color": "red" }} />
+        </GeoJSON>
+      {/if}
+    </MapLibre>
   </div>
 </Layout>
