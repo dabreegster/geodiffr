5eddf6a569f82782ace9fe2af9eabf194cc5bc0a
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-04-12 16:20:32 +0100

    Add diff type and sign

--- geodiffr-rs/src/lib.rs
+++ geodiffr-rs/src/lib.rs
@@ -1,25 +1,76 @@
 use std::path::Path;
 
 use geojson::{Feature, FeatureCollection, GeoJson};
+use serde::{Deserialize, Serialize};
 
 pub fn read_geojson<P: AsRef<Path>>(path: P) -> anyhow::Result<GeoJson> {
     let geojson_str = std::fs::read_to_string(path)?;
     Ok(geojson_str.parse::<GeoJson>()?)
 }
 
-pub fn diff_geojson(
-    geojson1: &GeoJson,
-    geojson2: &GeoJson,
-) -> anyhow::Result<(GeoJson, usize, usize)> {
+#[derive(Debug, Serialize, Deserialize)]
+/// An enum for the types of difference.
+enum DiffType {
+    Feature,
+    Source,
+    Geometry,
+    Position,
+    Semantic,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+/// An enum for whether a difference is an addition or deletion.
+enum DiffSign {
+    Addition,
+    Deletion,
+}
+
+const DIFF_KEY: &str = "diff";
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Diff {
+    r#type: DiffType,
+    sign: DiffSign,
+}
+
+pub fn diff_geojson(old: &GeoJson, new: &GeoJson) -> anyhow::Result<(GeoJson, usize, usize)> {
     let mut match_count = 0;
     let mut diff_count = 0;
     let mut diff_features: Vec<Feature> = Vec::new();
-    match (geojson1, geojson2) {
+    match (old, new) {
         (GeoJson::FeatureCollection(old), GeoJson::FeatureCollection(new)) => {
+            // Get deletions
             for feature in &old.features {
                 if !new.features.contains(feature) {
                     diff_count += 1;
-                    diff_features.push(feature.clone());
+                    let change = Diff {
+                        r#type: DiffType::Feature,
+                        sign: DiffSign::Deletion,
+                    };
+                    let mut map = serde_json::Map::new();
+                    map.insert(DIFF_KEY.to_string(), serde_json::to_value(&change)?);
+                    diff_features.push(Feature {
+                        foreign_members: Some(map),
+                        ..feature.clone()
+                    });
+                } else {
+                    match_count += 1;
+                }
+            }
+            // Get additions
+            for feature in &new.features {
+                if !old.features.contains(feature) {
+                    diff_count += 1;
+                    let change = Diff {
+                        r#type: DiffType::Feature,
+                        sign: DiffSign::Addition,
+                    };
+                    let mut map = serde_json::Map::new();
+                    map.insert(DIFF_KEY.to_string(), serde_json::to_value(&change)?);
+                    diff_features.push(Feature {
+                        foreign_members: Some(map),
+                        ..feature.clone()
+                    });
                 } else {
                     match_count += 1;
                 }
@@ -53,8 +104,8 @@ mod tests {
         let (geojson, match_count, diff_count) = diff_geojson(&old_geojson, &new_geojson).unwrap();
         println!("{}", geojson);
         println!("Match count: {match_count}");
-        assert_eq!(match_count, 1048);
+        assert_eq!(match_count, 2096);
         println!("Diff  count: {diff_count}");
-        assert_eq!(diff_count, 131);
+        assert_eq!(diff_count, 267);
     }
 }
