5f81c29461080fa9df25c14531e8ac0985d7af90
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-04-12 12:22:41 +0100

    Add simple cli

--- geodiffr-rs/src/bin/main.rs
+++ geodiffr-rs/src/bin/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello world!");
+use geodiffr_rs::{diff_geojson, read_geojson};
+
+fn main() -> anyhow::Result<()> {
+    let old_path = std::env::args().nth(1).expect("no old path given");
+    let new_path = std::env::args().nth(2).expect("no new path given");
+
+    let old = read_geojson(old_path)?;
+    let new = read_geojson(new_path)?;
+
+    let (geojson, _, _) = diff_geojson(&old, &new)?;
+    println!("{}", geojson);
+    Ok(())
 }
